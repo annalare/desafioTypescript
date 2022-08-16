cc00d2a9132c6d42366c4c9343aa51b052a30591
Larissa Mendes <83883968+annalare@users.noreply.github.com>
2022-08-15 22:50:57 -0300

    feat:types

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+//questão 1
+const primeiroNome = "seu nome";
+const diaDoMes = 12;
+const hojeFezSol = true;
---
+++ index.ts
@@ -0,0 +1,21 @@
+//questão 1
+const primeiroNome: string = "seu nome";
+const diaDoMes: number = 12;
+const hojeFezSol: boolean = true;
+
+//questao 2
+
+type Cachorrro = {
+  nome: string;
+  idade: number;
+  raça: string;
+  grandePorte: boolean;
+  donos: string;
+};
+
+interface Animais {
+  tipo: string;
+  bipede: boolean;
+  asas: boolean;
+  região: string;
+}
